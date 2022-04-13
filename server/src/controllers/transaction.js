4199c94d1b4386720d9dc8eeb74c21ed490b3613
Jody-septiawan <jody.septiawan5@gmail.com>
2022-04-13 14:50:32 +0700

    update

--- server/src/controllers/transaction.js
+++ server/src/controllers/transaction.js
@@ -1,6 +1,6 @@
-const { user, transaction, product, profile } = require("../../models");
+const { user, transaction, product, profile } = require('../../models');
 
-// Import midtransClient here ...
+const midtransClient = require('midtrans-client');
 
 exports.getTransactions = async (req, res) => {
   try {
@@ -9,37 +9,37 @@ exports.getTransactions = async (req, res) => {
       where: {
         idBuyer,
       },
-      order: [["createdAt", "DESC"]],
+      order: [['createdAt', 'DESC']],
       attributes: {
-        exclude: ["updatedAt", "idBuyer", "idSeller", "idProduct"],
+        exclude: ['updatedAt', 'idBuyer', 'idSeller', 'idProduct'],
       },
       include: [
         {
           model: product,
-          as: "product",
+          as: 'product',
           attributes: {
             exclude: [
-              "createdAt",
-              "updatedAt",
-              "idUser",
-              "qty",
-              "price",
-              "desc",
+              'createdAt',
+              'updatedAt',
+              'idUser',
+              'qty',
+              'price',
+              'desc',
             ],
           },
         },
         {
           model: user,
-          as: "buyer",
+          as: 'buyer',
           attributes: {
-            exclude: ["createdAt", "updatedAt", "password", "status"],
+            exclude: ['createdAt', 'updatedAt', 'password', 'status'],
           },
         },
         {
           model: user,
-          as: "seller",
+          as: 'seller',
           attributes: {
-            exclude: ["createdAt", "updatedAt", "password", "status"],
+            exclude: ['createdAt', 'updatedAt', 'password', 'status'],
           },
         },
       ],
@@ -58,35 +58,71 @@ exports.getTransactions = async (req, res) => {
     });
 
     res.send({
-      status: "success",
+      status: 'success',
       data,
     });
   } catch (error) {
     console.log(error);
     res.send({
-      status: "failed",
-      message: "Server Error",
+      status: 'failed',
+      message: 'Server Error',
     });
   }
 };
 
 exports.addTransaction = async (req, res) => {
   try {
-    // Prepare transaction data from body here ...
+    let data = req.body;
+    data = {
+      id: parseInt(data.idProduct + Math.random().toString().slice(3, 8)),
+      ...data,
+      idBuyer: req.user.id,
+      status: 'pending',
+    };
 
-    // Insert transaction data here ...
+    const newData = await transaction.create(data);
 
-    // Get buyer data here ...
+    const buyerData = await user.findOne({
+      include: {
+        model: profile,
+        as: 'profile',
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'idUser'],
+        },
+      },
+      where: {
+        id: newData.idBuyer,
+      },
+      attributes: {
+        exclude: ['createdAt', 'updatedAt', 'password'],
+      },
+    });
 
-    // Create Snap API instance here ...
+    let snap = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: process.env.MIDTRANS_SERVER_KEY,
+    });
 
-    // Create parameter for Snap API here ...
+    let parameter = {
+      transaction_details: {
+        order_id: newData.id,
+        gross_amount: newData.price,
+      },
+      credit_card: {
+        secure: true,
+      },
+      customer_details: {
+        full_name: buyerData?.name,
+        email: buyerData?.email,
+        phone: buyerData?.profile?.phone,
+      },
+    };
 
-    // Create trasaction token & redirect_url with snap variable here ...
+    const payment = await snap.createTransaction(parameter);
 
     res.send({
-      status: "pending",
-      message: "Pending transaction payment gateway",
+      status: 'pending',
+      message: 'Pending transaction payment gateway',
       payment,
       product: {
         id: data.idProduct,
@@ -95,13 +131,22 @@ exports.addTransaction = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.send({
-      status: "failed",
-      message: "Server Error",
+      status: 'failed',
+      message: 'Server Error',
     });
   }
 };
 
-// Create configurate midtrans client with CoreApi here ...
+const MIDTRANS_CLIENT_KEY = process.env.MIDTRANS_CLIENT_KEY;
+const MIDTRANS_SERVER_KEY = process.env.MIDTRANS_SERVER_KEY;
+
+const core = new midtransClient.CoreApi();
+
+core.apiConfig.set({
+  isProduction: false,
+  serverKey: MIDTRANS_SERVER_KEY,
+  clientKey: MIDTRANS_CLIENT_KEY,
+});
 
 /**
  *  Handle update transaction status after notification
@@ -110,8 +155,80 @@ exports.addTransaction = async (req, res) => {
  * @param {transactionId} transactionId
  */
 
-// Create function for handle https notification / WebHooks of payment status here ...
+exports.notification = async (req, res) => {
+  try {
+    const statusResponse = await core.transaction.notification(req.body);
 
-// Create function for handle transaction update status here ...
+    console.log('------- Notification --------- ✅');
+    console.log(statusResponse);
 
-// Create function for handle product update stock/qty here ...
+    const orderId = statusResponse.order_id;
+    const transactionStatus = statusResponse.transaction_status;
+    const fraudStatus = statusResponse.fraud_status;
+
+    if (transactionStatus == 'capture') {
+      if (fraudStatus == 'challenge') {
+        // TODO set transaction status on your database to 'challenge'
+        // and response with 200 OK
+        updateTransaction('pending', orderId);
+        res.status(200);
+      } else if (fraudStatus == 'accept') {
+        // TODO set transaction status on your database to 'success'
+        // and response with 200 OK
+        updateProduct(orderId);
+        updateTransaction('success', orderId);
+        res.status(200);
+      }
+    } else if (transactionStatus == 'settlement') {
+      // TODO set transaction status on your database to 'success'
+      // and response with 200 OK
+      updateTransaction('success', orderId);
+      res.status(200);
+    } else if (
+      transactionStatus == 'cancel' ||
+      transactionStatus == 'deny' ||
+      transactionStatus == 'expire'
+    ) {
+      // TODO set transaction status on your database to 'failure'
+      // and response with 200 OK
+      updateTransaction('failed', orderId);
+      res.status(200);
+    } else if (transactionStatus == 'pending') {
+      // TODO set transaction status on your database to 'pending' / waiting payment
+      // and response with 200 OK
+      updateTransaction('pending', orderId);
+      res.status(200);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+};
+
+const updateTransaction = async (status, transactionId) => {
+  await transaction.update(
+    {
+      status,
+    },
+    {
+      where: {
+        id: transactionId,
+      },
+    }
+  );
+};
+
+const updateProduct = async (orderId) => {
+  const transactionData = await transaction.findOne({
+    where: {
+      id: orderId,
+    },
+  });
+  const productData = await product.findOne({
+    where: {
+      id: transactionData.idProduct,
+    },
+  });
+  const qty = productData.qty - 1;
+  await product.update({ qty }, { where: { id: productData.id } });
+};
